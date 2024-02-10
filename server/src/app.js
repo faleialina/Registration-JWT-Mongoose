7f9309f4c1e7385decded06dbb454584236044f0
a.falei <august3@mail.ru>
2024-02-10 23:41:47 +0300

    swagger

--- server/src/app.js
+++ server/src/app.js
@@ -1,11 +1,13 @@
 const express = require('express');
 const bodyParser = require('body-parser');
-
+const swaggerUi = require('swagger-ui-express');
+const swaggerDocs = require('../swagger.json');
 const routeUser = require('./controller/user.controller');
 
 const app = express();
 
-app.use(bodyParser.json())
+app.use(bodyParser.json());
+app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocs));
 app.use('/user', routeUser);
 app.use((error, req, res, _next) => res.send(error.message));
 
